bf671f95beb29f1b21af5aa9b0c2c01a577c8d6e
Ahmed Mossa <ahmed.mossa@mail.mcgill.ca>
2025-01-28 19:09:06 -0500

    Finished the python script for Section 4

--- lab2-code-sg2/project/collect_color_sensor_data.py
+++ lab2-code-sg2/project/collect_color_sensor_data.py
@@ -6,21 +6,50 @@ It must be run on the robot.
 """
 
 # Add your imports here, if any
-from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor
+from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
+from time import sleep
 
 
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
-_ = EV3ColorSensor(...)
-_ = TouchSensor(...)
+COLOR_SENSOR = EV3ColorSensor(1)  # Port 1 for the color sensor, yet we can change this in the lab
+TOUCH_SENSOR = TouchSensor(2)     # Port 2 for the touch sensor
 
-wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
-
+wait_ready_sensors(True)  # Input True to see what the robot is trying to initialize! False to be silent.
 
 def collect_color_sensor_data():
     "Collect color sensor data."
-    ...
+    try:
+        output_file = open(COLOR_SENSOR_DATA_FILE, "w")
+        output_file.write("R,G,B\n")  # Write CSV header
+
+        print("Waiting for the first touch sensor press to start data collection.")
+        while not TOUCH_SENSOR.is_pressed():
+            pass  # Wait for the first button press
+
+        print("Touch sensor pressed. Starting to collect RGB data points.")
+        sleep(1)  # Small delay to avoid accidental double presses
+
+        while not TOUCH_SENSOR.is_pressed():  # Continue until another press ends collection
+            if TOUCH_SENSOR.is_pressed():
+                # Read RGB data from the color sensor
+                rgb_data = COLOR_SENSOR.get_rgb()  # Returns (R, G, B)
+                r, g, b = rgb_data
+                print(f"RGB: {r}, {g}, {b}") # to keep track of the data in the console
+                output_file.write(f"{r},{g},{b}\n")  # Write RGB to file
+
+                # Wait for the button to be released
+                while TOUCH_SENSOR.is_pressed():
+                    pass  # Debounce to prevent duplicate readings
+        print("Touch sensor pressed again. Stopping data collection.")
+    except BaseException: # capture all exceptions including KeyboardInterrupt (Ctrl-C)
+        pass 
+    finally:
+        print("Done collecting RGB data.")
+        output_file.close()  # Close the file
+        reset_brick()  # Reset hardware on the brick
+        exit()
 
 
 if __name__ == "__main__":
