8f7dc44b84d4bb5a811b3faaed9b8bcede1eac03
joseph050221 <josephzhangce0221@gmail.com>
2025-01-31 13:53:49 -0500

    Testing color sensor file without touch sensor attached

--- lab2-code-sg1/project/collect_color_sensor_data.py
+++ lab2-code-sg1/project/collect_color_sensor_data.py
@@ -26,21 +26,21 @@ def collect_color_sensor_data():
         
         print("Waiting for the first touch sensor press to start data collection.")
 
-        while not TOUCH_SENSOR.is_pressed():
-            pass  # Wait for the first button press
+        #while not TOUCH_SENSOR.is_pressed():
+            #pass  # Wait for the first button press
         
         #sleep(1)
         
         while True :
-            if TOUCH_SENSOR.is_pressed():
+            #if TOUCH_SENSOR.is_pressed():
                 # Read RGB data from the color sensor
                 rgb_data = COLOR_SENSOR.get_rgb()  # Returns (R, G, B)
                 r, g, b = rgb_data
                 print(f"RGB: {r}, {g}, {b}") # to keep track of the data in the console
                 output_file.write(f"{r},{g},{b}\n")  # Write RGB to file
                 #sleep(1)  # Small delay to avoid accidental double presses
-                while TOUCH_SENSOR.is_pressed():
-                        pass  # Debounce to prevent duplicate readings
+               # while TOUCH_SENSOR.is_pressed():
+                     #   pass  # Debounce to prevent duplicate readings
 
     except BaseException: # capture all exceptions including KeyboardInterrupt (Ctrl-C)
         pass 
