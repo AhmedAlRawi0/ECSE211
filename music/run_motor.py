0d2773eabb9fe707b68cc233f1eb4703aa1be261
BongoThirteen <50809853+BongoThirteen@users.noreply.github.com>
2025-02-14 19:16:45 +0000

    Fix run_motor.py

--- music/run_motor.py
+++ music/run_motor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from utils.sound import Sound
-from utils.brick import TouchSensor, EV3UltrasonicSensor, wait_ready_sensors, reset_brick
+from utils.brick import TouchSensor, EV3UltrasonicSensor, Motor, wait_ready_sensors, reset_brick
 from time import sleep
 
 RUN_BUTTON = TouchSensor(1)
