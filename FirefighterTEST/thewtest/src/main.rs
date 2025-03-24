0679d162c34da0db5d27670238e7530ec7aac4d5
Matthew Hoge <mailbox@bongothirteen.net>
2025-03-24 15:57:35 -0400

    added more testing code

--- FirefighterTEST/thewtest/src/main.rs
+++ FirefighterTEST/thewtest/src/main.rs
@@ -1,7 +1,34 @@
-use brickpi::BrickPi;
+use brickpi3::{BrickPi3, MotorPort, SensorError, SensorPort, SensorType};
 
 fn main() -> anyhow::Result<()> {
-    let brickpi = BrickPi::open("/dev/spidev0.1")?;
+    let mut brickpi = BrickPi3::open("/dev/spidev0.1")?;
+
+    brickpi.set_sensor_type(SensorPort::Port3, SensorType::Touch, 0)?;
+
+    print!("Configuring sensors... ");
+    while brickpi.read_sensor(SensorPort::Port3) == Err(SensorError::NotConfigured) {}
+    println!("done.");
+
+    brickpi.set_motor_position_relative(MotorPort::PortA, 179)?;
+    brickpi.set_motor_position_relative(MotorPort::PortB, -179)?;
+
+    loop {
+        let SensorData::Touch { pressed } = brickpi
+            .read_sensor(SensorPort::Port3)
+            .map_err(|e| anyhow!("{e:#}"))?
+        else {
+            bail!("invalid sensor data")
+        };
+        if pressed {
+            println!("Cancelled.");
+            break;
+        }
+    }
+
+    brickpi.set_motor_power(MotorPort::PortA, 0)?;
+    brickpi.set_motor_power(MotorPort::PortB, 0)?;
 
     println!("Hello, BrickPi!");
+
+    Ok(())
 }
