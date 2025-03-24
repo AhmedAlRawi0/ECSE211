0092a2e7cf77f57aafe4825b9e37e22b7d1aa486
Matthew Hoge <mailbox@bongothirteen.net>
2025-03-24 16:02:23 -0400

    changed sensor port

--- FirefighterTEST/thewtest/src/main.rs
+++ FirefighterTEST/thewtest/src/main.rs
@@ -4,10 +4,10 @@ use brickpi3::{BrickPi3, MotorPort, SensorData, SensorError, SensorPort, SensorT
 fn main() -> anyhow::Result<()> {
     let mut brickpi = BrickPi3::open("/dev/spidev0.1")?;
 
-    brickpi.set_sensor_type(SensorPort::Port3, SensorType::Touch, 0)?;
+    brickpi.set_sensor_type(SensorPort::Port4, SensorType::Touch, 0)?;
 
     print!("Configuring sensors... ");
-    while let Err(SensorError::NotConfigured) = brickpi.read_sensor(SensorPort::Port3) {}
+    while let Err(SensorError::NotConfigured) = brickpi.read_sensor(SensorPort::Port4) {}
     println!("done.");
 
     brickpi.set_motor_position_relative(MotorPort::PortA, 179)?;
@@ -15,7 +15,7 @@ fn main() -> anyhow::Result<()> {
 
     loop {
         let SensorData::Touch { pressed } = brickpi
-            .read_sensor(SensorPort::Port3)
+            .read_sensor(SensorPort::Port4)
             .map_err(|e| anyhow!("{e:?}"))?
         else {
             bail!("invalid sensor data")
