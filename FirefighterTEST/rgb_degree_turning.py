773dcf801aa8510bfb65aaaa8e5dd75757e590a0
Matthew Hoge <matthew.hoge@mail.mcgill.ca>
2025-03-30 17:38:22 -0400

    added encoder offset to rgb_degree_turning and fixed incorrect variable names

--- FirefighterTEST/rgb_degree_turning.py
+++ FirefighterTEST/rgb_degree_turning.py
@@ -62,6 +62,9 @@ def rotate_robot(angle: int) -> None:
         left_power = -30
         right_power = 30
 
+    left_init_pos = LEFT_DRIVE.get_position()
+    right_init_pos = RIGHT_DRIVE.get_position()
+
     LEFT_DRIVE.set_power(left_power)
     RIGHT_DRIVE.set_power(left_power)
 
@@ -70,20 +73,20 @@ def rotate_robot(angle: int) -> None:
     right_moving = True
     right_slow = False
     while True:
-        if left_moving and abs(LEFT_MOTOR.get_position()) > max(0, abs(wheel_angle) - 20):
-            LEFT_MOTOR.set_power(left_power // 2)
+        if left_moving and abs(LEFT_DRIVE.get_position() - left_init_pos) > max(0, abs(wheel_angle) - 20):
+            LEFT_DRIVE.set_power(left_power // 2)
             left_moving = False
             left_slow = True
-        if right_moving and abs(RIGHT_MOTOR.get_position()) > max(0, abs(wheel_angle) - 20):
-            RIGHT_MOTOR.set_power(right_power // 2)
+        if right_moving and abs(RIGHT_DRIVE.get_position() - right_init_pos) > max(0, abs(wheel_angle) - 20):
+            RIGHT_DRIVE.set_power(right_power // 2)
             right_moving = False
             right_slow = True
 
-        if left_slow and abs(LEFT_MOTOR.get_position()) > abs(wheel_angle):
-            LEFT_MOTOR.set_power(0)
+        if left_slow and abs(LEFT_DRIVE.get_position() - left_init_pos) > abs(wheel_angle):
+            LEFT_DRIVE.set_power(0)
             left_slow = False
-        if right_slow and abs(RIGHT_MOTOR.get_position()) > abs(wheel_angle):
-            RIGHT_MOTOR.set_power(0)
+        if right_slow and abs(RIGHT_DRIVE.get_position() - right_init_pos) > abs(wheel_angle):
+            RIGHT_DRIVE.set_power(0)
             right_slow = False
 
         if (not left_moving and not right_moving
